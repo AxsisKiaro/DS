a1b9e63376a9a79141005407d5d5472c8870f35b
AxsisKiaro <axsiskiaro@gmail.com>
2023-07-06 01:07:50 +0700

    Add ipynb

--- game_v2.py
+++ game_v2.py
@@ -5,7 +5,7 @@ def random_predict(number:int=1) -> int:
     """Рандомно угадываем число
 
     Args:
-        number (int, optional): Загаданное число. Defaults to 1. test
+        number (int, optional): Загаданное число. Defaults to 1.
 
     Returns:
         int: Число попыток
