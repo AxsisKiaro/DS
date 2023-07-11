83f2f9d24c41f696d78ccc03059591eb8c85287f
AxsisKiaro <axsiskiaro@gmail.com>
2023-07-11 12:26:31 +0700

    Create a new project_1

---
+++ project_1/guess_the_number.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+def score_game(random_predict) -> int:
+    """За какое количество попыток в среднем за 10000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(10000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попытки(-ок)")
+
+
+def game_core_v3(number: int = 1) -> int:
+    """
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    
+    count = 0
+    lower_bound = 1
+    upper_bound = 100
+    
+    while True:
+        count += 1
+        predict = (lower_bound + upper_bound) // 2
+        
+        if predict == number:
+            break
+
+        if predict < number:
+            lower_bound = predict + 1
+        else:
+            upper_bound = predict - 1
+
+    return count
+
+if __name__ == '__main__':
+    score_game(game_core_v3)
+    
