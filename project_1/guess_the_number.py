749e7b79c7dbe69c92c913b3d189cf745d5fad34
AxsisKiaro <axsiskiaro@gmail.com>
2023-07-11 12:37:51 +0700

    add comments, edit ipynb

--- project_1/guess_the_number.py
+++ project_1/guess_the_number.py
@@ -29,24 +29,24 @@ def game_core_v3(number: int = 1) -> int:
         int: Число попыток
     """
     
-    count = 0
-    lower_bound = 1
-    upper_bound = 100
+    count = 0 #Счетчик попыток
+    lower_bound = 1 #Начальное значение интервала
+    upper_bound = 100 #Конечное значение интервала
     
-    while True:
+    while True: 
         count += 1
-        predict = (lower_bound + upper_bound) // 2
+        predict = (lower_bound + upper_bound) // 2 #Переменная для предположительного результата отгадывания
         
-        if predict == number:
+        if predict == number: #если угадали - выходим из цикла
             break
 
-        if predict < number:
-            lower_bound = predict + 1
+        if predict < number: 
+            lower_bound = predict + 1 #если предположительный результат меньше, то присваиваем новое начальное значение интервала деленное на 2, +1
         else:
-            upper_bound = predict - 1
+            upper_bound = predict - 1#если предположительный результат больше, то присваиваем новое конечное значение интервала деленное на 2, -1
 
     return count
 
 if __name__ == '__main__':
-    score_game(game_core_v3)
+    score_game(game_core_v3) #для импорта в ipynb
     
